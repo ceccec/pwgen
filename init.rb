3ea9ff97cb6470227d915bb957035ad62bb14965
Tsvetan Roustchev <tsv@hitsol.net>
2011-10-23 21:49:39 +0300

    Facebook

--- init.rb
+++ init.rb
@@ -9,3 +9,13 @@ end
 get '/' do
   erb :index
 end
+
+# used by Canvas apps - redirect the POST to be a regular GET
+post "/" do
+  redirect "/"
+end
+
+# used to close the browser window opened to post to wall/send to friends
+get "/close" do
+  "<body onload='window.close();'/>"
+end
